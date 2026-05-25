e6c3d37783231d8ec844f94475d02fb7a3b06c0f
Mateusz <mateuszhorabik31@gmail.com>
2026-05-25 10:28:52 +0200

    Create table categories

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,8 +1,4 @@
-CREATE TABLE customers (
-    customer_id INT IDENTITY(1,1) PRIMARY KEY,
-    first_name NVARCHAR(50) NOT NULL,
-    last_name NVARCHAR(50) NOT NULL,
-    email NVARCHAR(100) NOT NULL,
-    city NVARCHAR(50) NOT NULL,
-    registration_date DATE NOT NULL
+CREATE TABLE categories (
+    category_id INT IDENTITY(1,1) PRIMARY KEY,
+    category_name NVARCHAR(100) NOT NULL
 );
