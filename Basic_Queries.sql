0c03eed2aabbc58be110bbd0641fa5fcc8e8d7d6
Mateusz <mateuszhorabik31@gmail.com>
2026-05-25 10:40:21 +0200

    Sales by category

--- Basic_Queries.sql
+++ Basic_Queries.sql
@@ -1,12 +1,13 @@
-Select TOP 10 
-p.product_id, p.product_name,oi.unit_price,
-SUM(oi.quantity) AS total_quantity_sold,
-Sum(oi.quantity * oi.unit_price) as total_income
-from products p
+Select 
+c.category_id, c.category_name,
+SUM(oi.quantity * oi.unit_price) AS category_revenue
+from categories c
+join products p
+on c.category_id = p.category_id
 join order_items oi
 on p.product_id = oi.product_id
 join orders o
-on o.order_id = oi.order_id
-where o.ordate_status = 'completed'
-group by p.product_id, p.product_name, oi.unit_price
-order by total_income,total_quantity_sold asc
+on oi.order_id = o.order_id
+where ordate_status = 'completed'
+group by c.category_id, c.category_name
+order by category_revenue desc
