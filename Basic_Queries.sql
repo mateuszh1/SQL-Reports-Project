7c421bbb257a0ae115acb0a0b217fe546e3a8a48
Mateusz <mateuszhorabik31@gmail.com>
2026-05-25 10:39:45 +0200

    Top products by revenue and Top products by quantity sold

--- Basic_Queries.sql
+++ Basic_Queries.sql
@@ -1,9 +1,12 @@
-use shop;
-Select MONTH(o.order_date) AS sales_month,
-sum(oi.quantity * oi.unit_price) as Total_Sales
-from orders o 
-join order_items oi 
+Select TOP 10 
+p.product_id, p.product_name,oi.unit_price,
+SUM(oi.quantity) AS total_quantity_sold,
+Sum(oi.quantity * oi.unit_price) as total_income
+from products p
+join order_items oi
+on p.product_id = oi.product_id
+join orders o
 on o.order_id = oi.order_id
 where o.ordate_status = 'completed'
-group by MONTH(o.order_date)
-order by sales_month asc
+group by p.product_id, p.product_name, oi.unit_price
+order by total_income,total_quantity_sold asc
