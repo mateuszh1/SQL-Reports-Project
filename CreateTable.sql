7bb8b7eff835b34693a7cb178372df050ef64c5d
Mateusz <mateuszhorabik31@gmail.com>
2026-05-25 10:33:53 +0200

    Add foreign key

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,4 +1,4 @@
-ALTER TABLE orders
-ADD CONSTRAINT FK_orders_customers
-FOREIGN KEY (customer_id)
-REFERENCES customers(customer_id);
+ALTER TABLE order_items
+ADD CONSTRAINT FK_order_items_products
+FOREIGN KEY (product_id)
+REFERENCES products(product_id);
