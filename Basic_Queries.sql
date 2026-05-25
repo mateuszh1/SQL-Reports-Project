63e481911e0abee4ad127916debe57c5f1cd14ca
Mateusz <mateuszhorabik31@gmail.com>
2026-05-25 10:39:09 +0200

    Monthly sales report

--- Basic_Queries.sql
+++ Basic_Queries.sql
@@ -1,12 +1,9 @@
 use shop;
-Select top 10 c.customer_id, c.first_name, c.last_name, c.email, c.city, 
-SUM(oi.quantity * oi.unit_price) as Total_Sales
-from customers c
-join orders o
-	on c.customer_id = o.customer_id
-join order_items oi
-	on o.order_id = oi.order_id
-where ordate_status = 'completed'
-group by 
- c.customer_id, c.first_name, c.last_name, c.email, c.city
- order by Total_Sales desc
+Select MONTH(o.order_date) AS sales_month,
+sum(oi.quantity * oi.unit_price) as Total_Sales
+from orders o 
+join order_items oi 
+on o.order_id = oi.order_id
+where o.ordate_status = 'completed'
+group by MONTH(o.order_date)
+order by sales_month asc
