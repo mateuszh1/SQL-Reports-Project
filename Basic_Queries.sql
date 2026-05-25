eb28361e4099ceacca83dce70d9d19753b1d660b
Mateusz <mateuszhorabik31@gmail.com>
2026-05-25 10:42:10 +0200

    Average order value

--- Basic_Queries.sql
+++ Basic_Queries.sql
@@ -1,5 +1,7 @@
-select o.ordate_status,count(oi.order_id) as orders_count from orders o
+with orders_sum as (
+select o.order_id, sum(oi.quantity * oi.unit_price) as order_price from orders o
 join order_items oi
 on o.order_id = oi.order_id
-group by o.ordate_status
-order by orders_count desc
+GROUP BY o.order_id
+)
+select avg(order_price) as average_order_value from orders_sum
